ece8044ef435ad0f32eb2b3edec19f0505a49bad
Sam Davies <sampdavies@gmail.com>
2017-09-20 09:59:00 +0100

    Custom resolvers (weight in lb)

--- lib/mma_web/schema/types.ex
+++ lib/mma_web/schema/types.ex
@@ -11,6 +11,11 @@ defmodule MmaWeb.Schema.Types do
     field :belts, :integer, description: "The number of belts a fighter currently holds"
     field :name, :string
     field :weight_in_kilos, :float, deprecate: "This will be replaced by weightInLbs"
+    field :weight_in_lbs, :float do
+      resolve fn _fighter = %{weight_in_kilos: weight_in_kilos}, _params, _info ->
+        {:ok, weight_in_kilos * 2.20462}
+      end
+    end
     field :fight_results, list_of(:fight_result), resolve: assoc(:fight_results)
   end
 
