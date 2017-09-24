a66d9209f53e06dd025f82ed5737d6fea24c5b5b
Sam Davies <sampdavies@gmail.com>
2017-09-24 19:38:51 +0100

    Query arguments

--- lib/mma_web/schema.ex
+++ lib/mma_web/schema.ex
@@ -3,6 +3,16 @@ defmodule MmaWeb.Schema do
   import_types MmaWeb.Schema.Types
 
   query do
+    field :fighter, :fighter do
+      arg :id, non_null(:id)
+
+      resolve fn %{id: id}, _info ->
+        case Mma.Repo.get(Mma.Fighter, id) do
+          nil -> {:error, :not_found}
+          fighter -> {:ok, fighter}
+        end
+      end
+    end
     field :fighters, list_of(:fighter) do
       resolve fn _params, _info ->
         {:ok, Mma.Repo.all(Mma.Fighter)}
